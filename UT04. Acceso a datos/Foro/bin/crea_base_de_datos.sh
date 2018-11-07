350aec7997f69881192713f1e38b71a4139488f6
NicoFQ-Clase <nicolasmatiasshm@hotmail.es>
2018-11-07 20:59:21 +0100

    Primeros avances en el foro.

---
+++ UT04. Acceso a datos/Foro/bin/crea_base_de_datos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BDUsuario = "admin_foro"
+BDNombreProyecto = "proyecto_foro"
+BDNombreBD = "proyecto_foro"
+BDContrasena = "1234"
+BDCreaTablas = "resources/creaTablas.sql"
+BDCreaDatos = "resources/creaDatos.sql"
+
+
