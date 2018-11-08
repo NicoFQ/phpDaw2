0b56357e9435b70eadbf97bc24416ab8f775c196
NicoFQ-Clase <nicolasmatiasshm@hotmail.es>
2018-11-08 21:11:55 +0100

    Scripts funcionando pero necesitan retoques.
    - Quitar comentarios de crear_base_de_datos.sh
    - Eliminar archivos de apoyo.

--- UT04. Acceso a datos/Foro/bin/crea_base_de_datos.sh
+++ UT04. Acceso a datos/Foro/bin/crea_base_de_datos.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-BDUsuario = "admin_foro"
-BDNombreProyecto = "proyecto_foro"
-BDNombreBD = "proyecto_foro"
-BDContrasena = "1234"
-BDCreaTablas = "resources/creaTablas.sql"
-BDCreaDatos = "resources/creaDatos.sql"
+BDUsuario="admin_foro"
+BDNombreProyecto="proyecto_foro"
+BDNombreBD="proyecto_foro"
+BDContrasena="1234"
+
+#BDBorraTablas="resources/borra_tablas.sql" 
+BDCreaTablas="resources/crea_tablas.sql"
+#BDCreaDatos="resources/crea_datos.sql"
+
+#mysql -u $BDUsuario -p$BDContrasena $BDNombreProyecto < $BDBorraTablas
+mysql -u $BDUsuario -p$BDContrasena $BDNombreProyecto < $BDCreaTablas
+#mysql -u $BDUsuario -p$BDContrasena $BDNombreProyecto < $BDCreaDatos
+#mysql -u $BDUsuario -p$BDContrasena $BDNombreProyecto < $BDBorraTablas
+
 
 
--- UT04. Acceso a datos/Foro/resources/crea_tablas.sql
+++ UT04. Acceso a datos/Foro/resources/crea_tablas.sql
@@ -1,38 +1,38 @@
+SET FOREIGN_KEY_CHECKS=0; 	-- QUITA LA COMPROBACION DE CLAVES FORANEAS EN MYSQL, 
+							
 DROP TABLE IF EXISTS usuario;
 CREATE TABLE usuario(
-	id_usuario INT(4) PRIMARY KEY,
+	id_usuario INT AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR(20) NOT NULL,
 	contrasena VARCHAR(20) NOT NULL
-	
 );
-
 
 
 DROP TABLE IF EXISTS tema;
 CREATE TABLE tema(
-	id_tema INT(4) PRIMARY KEY,
+	id_tema INT AUTO_INCREMENT PRIMARY KEY,
 	titulo VARCHAR(20),
 	etiqueta VARCHAR(20),
 	fecha_publcacion DATE NOT NULL,
-	id_usuario INT(4),
-	CONSTRAINT fk_tema FOREIGN KEY (id_usuario) 
+	id_usuario INT,
+	FOREIGN KEY (id_usuario) 
 	REFERENCES usuario(id_usuario)
-	ON DELETE cascade
-	);
+	ON DELETE CASCADE
+	) ENGINE=INNODB;
 
 
 DROP TABLE IF EXISTS respuesta;
 CREATE TABLE respuesta(
-	id_respuesta INT(4) PRIMARY KEY,
+	id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
 	titulo VARCHAR(20) NOT NULL,
 	usuario VARCHAR(20) NOT NULL,
-	contenido VARCHAR(20) NOT NULL,
+	contenido VARCHAR(200) NOT NULL,
 	fecha_publcacion DATE NOT NULL,
-	id_tema INT(4), 
-	CONSTRAINT fk_respuesta FOREIGN KEY (id_tema) 
+	id_tema INT, 
+	FOREIGN KEY (id_tema) 
 	REFERENCES tema(id_tema)
-	ON DELETE cascade
+	ON DELETE CASCADE
 	
-);
+) ENGINE=INNODB; -- INFORMACION DE ENGINE=INNODB -> https://www.arsys.es/blog/programacion/myisam-o-innodb-elige-tu-motor-de-almacenamiento-mysql/
 
 
