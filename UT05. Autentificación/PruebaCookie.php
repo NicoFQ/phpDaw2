2fe580f80e372722b812e2c133d9b7b7f14a73a5
NicoFQ-Clase <nicolasmatiasshm@hotmail.es>
2019-01-11 18:42:31 +0100

    Se añade autentificacion Web.

--- UT05. Autentificación/PruebaCookie.php
+++ UT05. Autentificación/PruebaCookie.php
@@ -1,5 +1,10 @@
 <?php 
-	print_r($_COOKIE);
-	setcookie("MiPrimeraCookie", "Hola mundo de las cookies");
-
+	//print_r($_COOKIE);
+	//print_r($_SERVER);
+	//setcookie("MiPrimeraCookie", "Hola mundo de las cookies");
+	if ($usuario = array(1,2,3,4)) {
+		echo "string";
+		}else{
+			echo " no string";
+		}
  ?>
